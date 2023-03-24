13e83c8b3e1a78f80295abe1d0f9cb8e82238e84
Sean Lacey <seanlacey2164@gmail.com>
2023-03-24 19:28:51 -0400

    submission of exercises from I / O chapter of intro to JS book

--- variables_exercises/age.js
+++ variables_exercises/age.js
@@ -5,7 +5,8 @@ reports the future age in 10, 20, 30 and 40 years. Below is the output for someo
 
 */
 
-let age = 20;
+let rlSync = require('readline-sync');
+let age = parseInt(rlSync.question('How old are you? '));
 console.log(`You are ${age} years old.`);
 console.log(`In 10 years, you will be ${age + 10} years old.`);
 console.log(`In 20 years, you will be ${age + 20} years old.`);
