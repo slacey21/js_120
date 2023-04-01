e163e62cce5436f9ce3b3cff6de5f10254632741
Sean Lacey <seanlacey2164@gmail.com>
2023-04-01 17:02:14 -0400

    submission of all exercises from Loops exercise set in JS Basics exercises

---
+++ javascript_basics_exercises/loops/1.js
@@ -0,0 +1,13 @@
+/*
+1. Add some code inside of the for loop below that logs the current 
+value of i to the console on each iteration. 
+Before you run the code: What sequence of numbers do you expect to be logged?
+*/
+
+/*
+0, 2, 4, 6, 8, 10
+*/
+
+for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
