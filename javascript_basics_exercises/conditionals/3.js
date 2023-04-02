e4e7d83cb38e93a7e452a0a4856954591684caa7
Sean Lacey <seanlacey2164@gmail.com>
2023-04-02 16:35:17 -0400

    conditionals exercises from JS Basics exercises

---
+++ javascript_basics_exercises/conditionals/3.js
@@ -0,0 +1,8 @@
+/*
+3. Take your code from the previous exercise and rewrite the conditional 
+so that it uses the ternary if-then-else operator.
+*/
+
+let randomNumber = Math.round(Math.random());
+
+console.log(randomNumber ? 'Yes!' : 'No.');
