0cee453317dd55f5f4d1c1c5d57eb0ae170c581b
Sean Lacey <seanlacey2164@gmail.com>
2023-04-02 20:44:55 -0400

    functions 2 exercises from JS basics exercise set

---
+++ javascript_basics_exercises/functions_2/8.js
@@ -0,0 +1,23 @@
+/*
+8. The function initGame below returns an object. 
+Refactor it using arrow function syntax.
+*/
+
+/*
+
+let initGame = function () {
+  return {
+    level: 1,
+    score: 0
+  }
+};
+
+*/
+
+let initGame = () => ({ level: 1, score: 0 });
+
+
+let game = initGame();
+
+console.log('Level: ' + game.level);
+console.log('Score: ' + game.score);
