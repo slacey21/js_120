89d499892921e3a57e9ea9fccd1a96339b6d2a28
Sean Lacey <seanlacey2164@gmail.com>
2023-04-10 22:04:19 -0400

    Adding language functionality to calcualtor program

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -4,6 +4,11 @@
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 
+// import dependencies
+const rlsync = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
+
+
 // checks whether a value is a valid number
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
@@ -14,9 +19,11 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-// import dependencies
-const rlsync = require('readline-sync');
-const messages = require('./calculator_messages.json');
+// function to get messages in chosen language
+function messages(message, lang) {
+  return MESSAGES[lang][message];
+}
+
 
 // init
 let anotherOp;
@@ -24,34 +31,44 @@ let anotherOp;
 // welcome message
 prompt(messages["welcome"]);
 
+// get language
+prompt("Language: ");
+let language = rlsync.question();
+
+//check if valid language
+while (!['es', 'en'].includes(language)) {
+  prompt("Please enter a valid language - English or Spanish: ");
+  language = rlsync.question();
+}
+
 do {
   // get user input for furst number in calculation
-  prompt(messages["getFirstNum"]);
+  prompt(messages("getFirstNum", language));
   let number1 = rlsync.question();
 
   // repeat prompt until user inputs a valid number
   while (invalidNumber(number1)) {
-    prompt(messages["invalidNumber"]);
+    prompt(messages("invalidNumber", language));
     number1 = rlsync.question();
   }
 
   // get user input for second number in calculation
-  prompt(messages["getSecondNum"]);
+  prompt(messages("getSecondNum", language));
   let number2 = rlsync.question();
 
   // repeat prompt until user inputs a valid number
   while (invalidNumber(number2)) {
-    prompt(messages["invalidNumber"]);
+    prompt(messages("invalidNumber", language));
     number2 = rlsync.question();
   }
 
   // ask user to choose operation type
-  prompt(messages["operationChoice"]);
+  prompt(messages("operationChoice", language));
   let operation = rlsync.question();
 
   // repeat prompt until user inputs a valid operation choice
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt(messages["invalidOperation"]);
+    prompt(messages("invalidOperation", language));
     operation = rlsync.question();
   }
 
@@ -75,15 +92,15 @@ do {
   }
 
   // output final result to the console
-  prompt(messages["result"]  + output);
+  prompt(messages("result", language)  + output);
 
   // prompt the user to perform another operation
-  prompt(messages["anotherOperation"]);
+  prompt(messages("anotherOperation", language));
   anotherOp = rlsync.question();
 
   // repeat prompt until user inputs a valid answer for another operation
   while (!['1', '2'].includes(anotherOp)) {
-    prompt(messages["invalidAnother"]);
+    prompt(messages("invalidAnother", language));
     anotherOp = rlsync.question();
   }
 } while (anotherOp === '1');
