eccce395240b498df776a4b785210c6fc8fbedea
Sean Lacey <92884504+slacey21@users.noreply.github.com>
2023-04-22 16:15:56 -0400

    Completed all easy 1 problems

---
+++ 2.js
@@ -0,0 +1,8 @@
+// How can you determine whether a given string ends with an exclamation mark (!)?
+
+let str1 = "Come over here!"; // true
+let str2 = "What's up, Doc?"; // false
+
+// ANSWER:
+str1.endsWith('!');
+str2.endsWith('!');
