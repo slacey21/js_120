b104e9fc9f753733947d7b351d2bb577e37895fe
Sean Lacey <92884504+slacey21@users.noreply.github.com>
2023-05-22 22:53:54 -0400

    Delete easy_2 directory

--- easy_2/9.js
+++
@@ -1,7 +0,0 @@
-/*
-Build a program that randomly generates Teddy's age, and logs it to the 
-console. Have the age be a random number between 20 and 120 (inclusive).
-*/
-
-
-console.log(`Teddy is ${Math.floor(Math.random() * 101) + 20} years old!`);
