9cfe1ba4870ba059a4bd950f646f4f4ad3776e57
Sean Lacey <seanlacey2164@gmail.com>
2023-08-05 15:00:12 -0400

    added easy 1 1-4

---
+++ easy_1/2.js
@@ -0,0 +1,7 @@
+// Log all odd numbers from 1 to 99, inclusive, to the console, with
+// each number on a separate line.
+
+for (let num = 1; num < 100; num += 2) {
+  console.log(num);
+}
+
---
+++ easy_1/3.js
@@ -0,0 +1,9 @@
+// Log all even numbers from 1 to 99, inclusive, to the console, with
+// each number on a separate line.
+
+for (let num = 1; num < 100; num++) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+}
+
