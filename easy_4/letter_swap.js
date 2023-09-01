906cb69d21ac8a6b3765daa2bfe73df01af42565
Sean Lacey <seanlacey2164@gmail.com>
2023-08-31 22:29:28 -0400

    submit final 6 easy_4 problems

---
+++ easy_4/letter_swap.js
@@ -0,0 +1,31 @@
+/*
+Given a string of words separated by spaces, write a function that
+swaps the first and last letters of every word.
+
+You may assume that every word contains at least one letter, and that
+the string will always contain at least one word. You may also assume that
+each string contains nothing but words and spaces, and that there are no
+leading, trailing, or repeated spaces.
+*/
+
+function swap(wordStr) {
+  let wordArr = wordStr.split(" ");
+  let endsSwapped = wordArr.map(word => {
+    let tempWord = word.split("");
+    let first = tempWord[0];
+    let last = tempWord[tempWord.length - 1];
+    tempWord[0] = last;
+    tempWord[tempWord.length - 1] = first;
+    return tempWord.join("");
+  });
+
+  let endsSwappedStr = endsSwapped.join(" ");
+
+  return endsSwappedStr;
+}
+
+
+console.log(swap('Oh what a wonderful day it is'));  // "hO thaw a londerfuw yad ti si"
+console.log(swap('Abcde'));                          // "ebcdA"
+console.log(swap('a'));                              // "a"
+
