f7e2d2d18552f338c76a6e26f4f65b8bb2024bc1
Sean Lacey <seanlacey2164@gmail.com>
2023-10-29 17:51:41 -0400

    completed classes exercises

---
+++ js_120/lesson_3/classes_practice/1.js
@@ -0,0 +1,8 @@
+/*
+1. What do we mean when we say that classes are first-class values
+
+We mean that, since classes are functions which are objects, classes can 
+be passed to functions as arguments, returned from functions, and assigned
+to values in JavaScript.
+
+*/
---
+++ js_120/lesson_3/classes_practice/2.js
@@ -0,0 +1,19 @@
+// Consider the following class declaration:
+
+class Television {
+  static manufacturer() {
+    // omitted code
+  }
+
+  model() {
+    // method logic
+  }
+}
+
+/*
+ANSWER:
+The static modifier adds a static method to the Television class
+itself.
+
+We would write: Television.manufacturer()
+*/
